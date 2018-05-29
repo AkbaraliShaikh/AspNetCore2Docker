4782687d8ae5e1a1c02b701b125e9826a4251898
AkbaraliShaikh <aashaikh55@gmail.com>
2018-05-30 00:29:22 +0530

    repo

---
+++ AspNetCore2Docker/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore2Docker.Controllers
+{
+    [Route("api/[controller]")]
+    public partial class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new Info("Asp.NEt Core 2.0 on Linux", "Docker makes life easy"));
+        }
+    }
+}
---
+++ AspNetCore2Docker/Info.cs
@@ -0,0 +1,13 @@
+﻿namespace AspNetCore2Docker
+{
+    public class Info
+    {
+        public Info(string name, string desc)
+        {
+            this.Name = name;
+            this.Desc = desc;
+        }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+    }
+}
